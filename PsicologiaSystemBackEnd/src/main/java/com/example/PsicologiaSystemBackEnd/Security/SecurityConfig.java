c35a02426fc2fc4d8511d5104b9b2dc268ecd758
gabriel.kaminise <gabriel.kaminise@sankhya.com.br>
2024-11-21 22:55:41 -0300

    Adicionando Login e autenticação de usuarios

---
+++ PsicologiaSystemBackEnd/src/main/java/com/example/PsicologiaSystemBackEnd/Security/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.example.PsicologiaSystemBackEnd.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/login").permitAll()
+                            .requestMatchers("/auth/login").permitAll();
+                    auth.anyRequest().authenticated();
+                });
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
